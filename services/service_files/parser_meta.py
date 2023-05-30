c9e1bfe4c09877e6868bf8e1d2b3198c6ef03558
Антон <theshick@mail.ru>
2023-05-30 12:22:34 +0300

    fix user_agent

--- services/service_files/parser_meta.py
+++ services/service_files/parser_meta.py
@@ -13,7 +13,9 @@ __all__ = ['FinamExportError', 'FinamDownloadError', 'FinamThrottlingError', 'Fi
            'FinamObjectNotFoundError', 'FinamTooLongTimeframeError', 'FinamAlreadyInProgressError']
 
 FINAM_CHARSET = 'cp1251'
-FINAM_TRUSTED_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) ' \
+# FINAM_TRUSTED_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) ' \
+#                            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+FINAM_TRUSTED_USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) ' \
                            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 FINAM_BASE = 'https://www.finam.ru'
 FINAM_ENTRY_URL = FINAM_BASE + '/profile/moex-akcii/gazprom/export/'
