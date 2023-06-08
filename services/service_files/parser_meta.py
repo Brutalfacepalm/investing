2e790e88b49b263a095434f41e6eb0293ab491d0
Антон <theshick@mail.ru>
2023-06-08 15:54:09 +0300

    fix parse_meta

--- services/service_files/parser_meta.py
+++ services/service_files/parser_meta.py
@@ -77,12 +77,11 @@ class FetchMetaWebriver:
             chrome_options = Options()
             # Basic driver`s options
             chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--headless')
+            # chrome_options.add_argument('--headless')
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument('--disable-translate')
             chrome_options.add_argument('--disable-extensions')
             chrome_options.add_argument('--disable-notifications')
-            # # The following options is mandatory if you are going to run it in docker container
             chrome_options.add_argument("--disable-gpu")
             # Disable images and css loading
             prefs = {
