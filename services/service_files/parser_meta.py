73f65a1e2145a85d397194748698cd08edc0a49a
Антон <theshick@mail.ru>
2023-06-08 16:42:15 +0300

    fixt driver

--- services/service_files/parser_meta.py
+++ services/service_files/parser_meta.py
@@ -79,10 +79,10 @@ class FetchMetaWebriver:
             chrome_options.add_argument('--no-sandbox')
             # chrome_options.add_argument('--headless')
             chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_options.add_argument('--disable-translate')
-            chrome_options.add_argument('--disable-extensions')
-            chrome_options.add_argument('--disable-notifications')
-            chrome_options.add_argument("--disable-gpu")
+            # chrome_options.add_argument('--disable-translate')
+            # chrome_options.add_argument('--disable-extensions')
+            # chrome_options.add_argument('--disable-notifications')
+            # chrome_options.add_argument("--disable-gpu")
             # Disable images and css loading
             prefs = {
                 "profile.managed_default_content_settings.images": 2,
