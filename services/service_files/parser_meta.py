fab74eb2dec0bfa4be1d7f675f62a62c04d68c93
Антон <theshick@mail.ru>
2023-06-08 17:02:52 +0300

    fix try

--- services/service_files/parser_meta.py
+++ services/service_files/parser_meta.py
@@ -77,15 +77,15 @@ class FetchMetaWebriver:
             chrome_service = Service(executable_path='/usr/local/bin/chromedriver')
             chrome_options = Options()
             # Basic driver`s options
-            # chrome_options.add_argument('--headless')
-            chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument("--disable-dev-shm-usage")
-            # chrome_options.add_argument("--remote-debugging-port=9222")
-            # chrome_options.add_argument(f'user-agent={FINAM_TRUSTED_USER_AGENT}')
             chrome_options.add_argument('--disable-translate')
             chrome_options.add_argument('--disable-extensions')
             chrome_options.add_argument('--disable-notifications')
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--headless')
             chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            # chrome_options.add_argument("--remote-debugging-port=9222")
+            # chrome_options.add_argument(f'user-agent={FINAM_TRUSTED_USER_AGENT}')
             # Disable images and css loading
             prefs = {
                 "profile.managed_default_content_settings.images": 2,
